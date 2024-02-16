9a841efaa97dd7b7f440821592529e52907e39c2
Morteza Rahimi <rahimi.morteza53266@gmail.com>
2024-02-16 21:58:43 +0330

    Chat with group configuration added.

--- androidApp/src/main/java/com/example/chatclient/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/chatclient/android/MainActivity.kt
@@ -3,37 +3,27 @@ package com.example.chatclient.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.chatclient.Greeting
 import io.ktor.client.HttpClient
-import io.ktor.client.plugins.api.Send
 import io.ktor.client.plugins.websocket.DefaultClientWebSocketSession
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.plugins.websocket.webSocket
-import io.ktor.http.HttpMethod
 import io.ktor.websocket.Frame
 import io.ktor.websocket.readText
 import io.ktor.websocket.send
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlin.reflect.KMutableProperty0
 
 class MainActivity : ComponentActivity() {
     val client by lazy {
@@ -43,6 +33,26 @@ class MainActivity : ComponentActivity() {
     }
 
     var msgFromServer by mutableStateOf("")
+    lateinit var defaultClientWebSocketSession:DefaultClientWebSocketSession
+
+    init {
+        CoroutineScope(Dispatchers.Main).launch {
+            client.webSocket(
+                method = io.ktor.http.HttpMethod.Get,
+                host = "192.168.199.34",
+                port = 8080,
+                path = "/chat"
+            ) {
+        defaultClientWebSocketSession = this
+
+                for (message in incoming) {
+                    message as? Frame.Text ?: continue
+                    println(message.readText())
+                    msgFromServer = msgFromServer + "\n" +message.readText()
+                }
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,29 +63,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
-                    GreetingView(actionSend = ::sendMessage,messageFrom = msgFromServer )
-
-                }
-            }
-        }
-    }
-
-
-    private fun sendMessage(messageToSend: String) {
-
-        CoroutineScope(Dispatchers.Main).launch {
-            client.webSocket(
-                method = HttpMethod.Get,
-                host = "192.168.126.96",
-                port = 8080,
-                path = "/chat"
-            ) {
-                send(messageToSend)
-                for (message in incoming) {
-                    message as? Frame.Text ?: continue
-                    println(message.readText())
-                    msgFromServer = message.readText()
+                    GreetingView(actionSend = ::defaultClientWebSocketSession, messageFrom = msgFromServer)
                 }
             }
         }
@@ -88,38 +76,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-
-suspend fun DefaultClientWebSocketSession.inputMessages() {
-
-    while (true) {
-        val message = readLine() ?: ""
-        if (message.equals("exit", true)) return
-        try {
-            send("Hi morteza")
-        } catch (e: Exception) {
-            println("Error while sending: " + e.localizedMessage)
-            return
-        }
-    }
-}
-
-
-fun DefaultClientWebSocketSession.outputMessages() {
-
-    CoroutineScope(Dispatchers.Main).launch {
-        try {
-            for (message in incoming) {
-                message as? Frame.Text ?: continue
-                println(message.readText())
-            }
-        } catch (e: Exception) {
-            println("Error while receiving: " + e.localizedMessage)
-        }
-    }
-}
-
 @Composable
-fun GreetingView(actionSend: (text: String) -> Unit, messageFrom: String? = null) {
+fun GreetingView(actionSend: KMutableProperty0<DefaultClientWebSocketSession>, messageFrom: String? = null) {
     var messageToSend by remember { mutableStateOf("") }
 
     Column(
@@ -132,7 +90,10 @@ fun GreetingView(actionSend: (text: String) -> Unit, messageFrom: String? = null
             value = messageToSend,
         )
 
-        Button(onClick = { actionSend(messageToSend) }, modifier = Modifier.fillMaxWidth()) {
+        Button(
+            onClick = { CoroutineScope(Dispatchers.Main).launch { actionSend.get().send(messageToSend) } },
+            modifier = Modifier.fillMaxWidth()
+        ) {
             Text(text = "Send")
         }
 
@@ -140,7 +101,6 @@ fun GreetingView(actionSend: (text: String) -> Unit, messageFrom: String? = null
         Text(text = messageFrom ?: "no reply")
 
     }
-
 }
 
 
